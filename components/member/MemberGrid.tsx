f02ba654c3b8f5d0b4e4bd09c1932cd766f52e2e
dawit-gt <dawatd111@gmail.com>
2026-07-13 22:15:38 +0300

    Fix MemberGrid type error

--- components/member/MemberGrid.tsx
+++ components/member/MemberGrid.tsx
@@ -1,37 +1,15 @@
 import MemberCard from "./MemberCard";
 import { Member } from "@/types/member";
 
-const members: Member[] = [
-  {
-    id: "1",
-    full_name: "Michiale Solomon",
-    age: 24,
-    gender: "Male",
-    phone: "0950236535",
-    status: "Employed",
-    photo_url: "/default-avatar.png",
-    created_at: "",
-  },
-  {
-    id: "2",
-    full_name: "Minase Maru",
-    age: 22,
-    gender: "Male",
-    phone: "0912345678",
-    status: "University Student",
-    photo_url: "/default-avatar.png",
-    created_at: "",
-  },
-];
+interface Props {
+  members: Member[];
+}
 
-export default function MemberGrid() {
+export default function MemberGrid({ members }: Props) {
   return (
-    <div className="mt-12 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {members.map((member) => (
-        <MemberCard
-          key={member.id}
-          member={member}
-        />
+        <MemberCard key={member.id} member={member} />
       ))}
     </div>
   );
